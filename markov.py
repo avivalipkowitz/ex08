531c064037a7d2c568ef7e47ca1837c1e6b7c2e1
Hackbright Student <info@hackbrightacademy.com>
2014-06-10 16:31:25 -0700

    completed basic code

--- markov.py
+++ markov.py
@@ -1,77 +1,78 @@
-# #!/usr/bin/env python
+#!/usr/bin/env python
 
+def make_chains(corpus):
+    """Takes an input text as a string and returns a dictionary of
+    markov chains."""
+    return {}
 
+def make_text(chains):
+    """Takes a dictionary of markov chains and returns random text
+    based off an original text."""
+    return "Here's some random text."
 
-# def make_chains(corpus):
-#     """Takes an input text as a string and returns a dictionary of
-#     markov chains."""
-#     return {}
+def main():
+    from sys import argv
+    from random import randint
 
-# def make_text(chains):
-#     """Takes a dictionary of markov chains and returns random text
-#     based off an original text."""
-#     return "Here's some random text."
+    input_file = argv[1]
+    myfile = open(input_file)
 
-# def main():
-from sys import argv
+    clean_text = myfile.read()
+    clean_text = clean_text.lower()
+    word_list = clean_text.split()
 
-input_file = argv[1]
-myfile = open(input_file)
-
-clean_text = ""
-
-for line in myfile:
-    line.rstrip()
-    line = line.lower()
-    for char in line:
-        #convert each char to string, and then do ascii conv
-        if ord(str(char)) ==10:
-            char == " "
-        elif ord(str(char)) >= 97 and ord(str(char)) <= 122:
-            clean_text += char
-        elif char == "":
-            pass
+    monogram = {}
+    for i in range(0, len(word_list)-1):
+        if monogram.has_key(word_list[i]):  #check to see if monogram dict already has this key from word_list
+            monogram[word_list[i]].append(word_list[i+1]) # if so, append the next word from word_list to monogram value
         else:
-            char = " "
-            clean_text += char
+            value = [word_list[i + 1]]
+            monogram.update({word_list[i]: value}) # if not, create new entry in dict
 
-word_list = clean_text.split(" ")
+    bigram = {}
 
-
-# monogram_count = {}
-# for word in word_list:
-#     if monogram_count.has_key(word):
-#         monogram_count[word] += 1
-#     else:
-#         monogram_count.update({word:1})  #monogram_count[word] = 1
-# del monogram_count['']
-# print monogram_count
-monogram = {}
-for i in range(0, len(word_list)-1):
-    if monogram.has_key(word_list[i]):  #check to see if monogram dict already has this key from word_list
-        monogram[word_list[i]].append(word_list[i+1]) # if so, append the next word from word_list to monogram value
-    else:
-        value = [word_list[i + 1]]
-        monogram.update({word_list[i]: value}) # if not, create new entry in dict
-# del monogram['']
-
-# bigram = {}
-# for i in range(0, len(word_list)):
-#     w1 = word_list[i]
-#     w2 = word_list[i + 1]
-#     w3 = word_list[i + 2]
-
-#     bigram = (w1, w2)
+    for i in range(0, len(word_list) - 3):
+        w1 = word_list[i]
+        w2 = word_list[i + 1]
+        w3 = word_list[i + 2]
+        if not (w1, w2) in bigram:
+            bigram[(w1, w2)] = [w3]
+        else: 
+            bigram[(w1,w2)].append(w3)
 
 
+        
+    monogram_key_list = monogram.keys()
 
+    word_1_index = randint(0,len(monogram.keys()) - 1)
+    rand_word_1 = monogram_key_list[word_1_index]
 
-    # Change this to read input_text from a file
-    # input_text = "Some text"
+    word_2_list = monogram[rand_word_1]
+    word_2_index = randint(0,len(word_2_list) - 1)
+    rand_word_2 = word_2_list[word_2_index]
 
-    # chain_dict = make_chains(input_text)
-    # random_text = make_text(chain_dict)
-    # print random_text
+    crazy_text_list = [rand_word_1, rand_word_2]
 
-# if __name__ == "__main__":
-#     main()
+    keep_adding = True
+    crazy_text_list_index = 0
+
+    while keep_adding:
+        w1 = crazy_text_list[crazy_text_list_index]
+        w2 = crazy_text_list[crazy_text_list_index+1]
+        bigram_pair = (w1,w2)
+
+        if bigram_pair in bigram:
+            word_3_index = randint(0, len(bigram[bigram_pair]) - 1)
+            rand_word_3 = bigram[bigram_pair][word_3_index]
+            crazy_text_list.append(rand_word_3)
+            crazy_text_list_index += 1
+        else:
+            keep_adding = False
+                
+    #    print crazy_text_list
+
+    crazy_text = " ".join(crazy_text_list)
+    print crazy_text
+
+if __name__ == "__main__":
+    main()
